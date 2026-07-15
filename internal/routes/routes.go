7a0e67f7e7183381892ca00a2470cc589a93fd92
whotterre <ijeddy5.0@gmail.com>
2026-07-15 06:34:48 +0100

    refactor(db): migrate repository logic from pgxpool to sqlc

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,15 +1,17 @@
 package routes
 
 import (
-	"github.com/Steve-s-Circle-on-System-Design/golang-rbac-system/internal/auth"
-	"github.com/Steve-s-Circle-on-System-Design/golang-rbac-system/internal/user"
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
+
+	"github.com/Steve-s-Circle-on-System-Design/golang-rbac-system/internal/auth"
+	"github.com/Steve-s-Circle-on-System-Design/golang-rbac-system/internal/users"
 )
 
 func SetupRoutes(db *pgxpool.Pool, app *gin.Engine) {
-	authRepository := user.NewRepository(db)
+	authRepository := users.NewRepository(db)
 	authService := auth.NewService(authRepository)
 	authHandlers := auth.NewHandler(authService)
 	app.POST("/auth/register", authHandlers.RegisterUser)
+	app.POST("/auth/login", authHandlers.LoginUser)
 }
